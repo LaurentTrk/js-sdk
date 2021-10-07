feae90419271fe52cbaad244c4f0f5c06e1400fb
Laurent Turek <laurent.turek@gmail.com>
2021-10-07 17:21:57 +0200

    Change layout and link buttons

--- packages/phapass-extension/components/Layout.tsx
+++ packages/phapass-extension/components/Layout.tsx
@@ -1,10 +1,7 @@
 import type {FC} from 'react'
-import Head from 'next/head'
-import {H2} from 'baseui/typography'
 import useIsClient from 'hooks/useIsClient'
 import {ToasterContainer} from 'baseui/toast'
 import {Block} from 'baseui/block'
-import AccountSelect from './AccountSelect'
 import {
   HeaderNavigation,
   ALIGN,
@@ -18,22 +15,16 @@ import {
 import { StyledLink } from "baseui/link";
 
 const Layout: FC<{title?: string}> = ({title, children}) => {
-  const isClient = useIsClient()
   const displayTitle = title || 'Phala SDK Example'
   return (
-    <Block width="100%" margin="0 auto" padding="0 10px">
-      {/* <Head>
-        <title>{displayTitle}</title>
-      </Head> */}
+    <Block maxWidth="1200px" width="100%" margin="0 auto" padding="0 10px">
       <HeaderNavigation>
         <StyledNavigationList $align={ALIGN.left}>
           <StyledNavigationItem>
-            <StyledLink onClick={ ()=>{ window.open("https://phala.network/en/", '_blank')}}>
+            <StyledLink href="https://phala.network/en/" 
+                        onClick={ (event)=>{ event.stopPropagation();event.preventDefault();window.open("https://phala.network/en/", '_blank')}}>
             <AspectRatioBox width="scale1400">
-              <AspectRatioBoxBody
-                as="img"
-                src="./phala-128.png"
-              />
+              <AspectRatioBoxBody as="img" src="./phala-128.png"/>
             </AspectRatioBox>
             </StyledLink>
           </StyledNavigationItem>
@@ -41,39 +32,10 @@ const Layout: FC<{title?: string}> = ({title, children}) => {
         </StyledNavigationList>
         <StyledNavigationList $align={ALIGN.center}/>
         <StyledNavigationList $align={ALIGN.right}>
-          <StyledNavigationItem>
-            <StyledLink onClick={ ()=>{ window.open("https://github.com/LaurentTrk/phapass", '_blank')}}>
-              <AspectRatioBox width="scale800">
-                <AspectRatioBoxBody
-                as="img"
-                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                />
-              </AspectRatioBox>
-            </StyledLink>
-          </StyledNavigationItem>
-          <StyledNavigationItem>
-            <StyledLink onClick={ ()=>{ window.open("https://polkadot.network/", '_blank')}}>
-              <AspectRatioBox width="scale800">
-                <AspectRatioBoxBody
-                as="img"
-                src="https://static-00.iconduck.com/assets.00/polkadot-cryptocurrency-icon-512x512-ik5ji7r8.png"
-                />
-              </AspectRatioBox>
-            </StyledLink>
-          </StyledNavigationItem>
-          <StyledNavigationItem>
-            <StyledLink onClick={ ()=>{ window.open("https://www.linkedin.com/in/laurenttrk/", '_blank')}}>
-              <AspectRatioBox width="scale1000">
-                <AspectRatioBoxBody
-                as="img"
-                src="https://www.nadacevinci.cz/wp-content/uploads/2020/04/linkedin-icon-for-email-23.png"
-                />
-              </AspectRatioBox>
-            </StyledLink>
-          </StyledNavigationItem>
-
-
-                      
+          <LinkButton linkUrl="https://github.com/LaurentTrk/phapass" imageUrl="./github.png"/>
+          <LinkButton linkUrl="https://www.encode.club/polkadot-club-hackathon" imageUrl="./encode.png"/>
+          <LinkButton linkUrl="https://polkadot.network/" imageUrl="./polkadot.png"/>
+          <LinkButton linkUrl="https://www.linkedin.com/in/laurenttrk/" imageUrl="./linkedin.png"/>
         </StyledNavigationList>
       
       </HeaderNavigation>
@@ -81,13 +43,11 @@ const Layout: FC<{title?: string}> = ({title, children}) => {
       <Block width="100%" maxWidth="1000px" margin="0 auto" padding="0 10px">
         <Block
           as="header"
-          height="120px"
+          height="60px"
           display="flex"
           alignItems="center"
           justifyContent="space-between"
         >
-          {/* <H2>{displayTitle}</H2> */}
-          {false && isClient && <AccountSelect></AccountSelect>}
         </Block>
 
         <main>{children}</main>
@@ -99,6 +59,16 @@ const Layout: FC<{title?: string}> = ({title, children}) => {
         />
       </Block>
     </Block>
+  )
+}
+
+const LinkButton: FC<{linkUrl: string, imageUrl: string}> = ({linkUrl, imageUrl}) => {
+  return (
+    <StyledNavigationItem>
+      <StyledLink href={linkUrl} onClick={ (event)=>{ event.stopPropagation();event.preventDefault();window.open(linkUrl, '_blank')}}>
+        <img style={{maxWidth:'32px'}} src={imageUrl}></img>
+      </StyledLink>
+    </StyledNavigationItem>
   )
 }
 
