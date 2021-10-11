0f6454a368ed6909399d1431e0bfa834da422269
Laurent Turek <laurent.turek@gmail.com>
2021-10-11 18:07:02 +0200

    [CLEAN] Delete atom vault keys

--- packages/phapass-extension/atoms/vaultPublicKey.ts
+++
@@ -1,9 +0,0 @@
-import { HexString } from '@polkadot/util/types'
-import {atomWithStorage} from 'jotai/utils'
-
-const vaultPublicKey = atomWithStorage<HexString | null>(
-  'atom:vaultPublicKey',
-  null
-)
-
-export default vaultPublicKey
